6b8d3e9150912b91b8179f68d88652887de6c15c
Vincent Martinetto <vmartinetto@ucmerced.edu>
2019-02-25 17:59:01 -0800

    commit

--- even_fibbonacci_numbers.f90
+++ even_fibbonacci_numbers.f90
@@ -1,3 +1,28 @@
        program even_fibonacci_numbers
 !
-       
+       implicit none
+       integer :: x=1,y=2,z=0
+!
+!
+!
+       do
+         if (mod(x,2).eq.0) then
+           z = z + x
+         end if
+         if (mod(y,2).eq.0) then
+           z = z + y
+         end if
+         x = x + y
+         y = x + y
+         if ((x.or.y).ge.4000000) then 
+           exit
+         end if 
+       end do 
+!
+!
+!
+       print *, z
+!
+!
+!
+       end program even_fibonacci_numbers
